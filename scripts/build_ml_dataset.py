67389e3ce7ace3ea0e4e9a912bd9e8b9a7b1b952
Mike Klein <kleinmichaeldavid@gmail.com>
2021-05-23 23:45:40 -0400

    add feature engineering

--- scripts/build_ml_dataset.py
+++ scripts/build_ml_dataset.py
@@ -1,16 +1,22 @@
 
+"""
+The purpose of this script is to create feature engineered variables (e.g. per-game stats)
+to be used in the ML model. These variables are saved in the mlfeatures table.
+"""
+
 ### SETUP ###
 
 import pandas as pd
 import numpy as np
 from copy import deepcopy
 from pathlib import Path
-from scripts.helper import create_database_connection
+from scripts.helper import create_database_connection, execute_query
 
 PATH_DB = Path('data/raw/nhl.db')
 PATH_DATA_PROCESSED = Path('data/processed')
+PATH_QUERIES = Path('queries')
 WEIGHT_PREV_SEASON = 10 # consider previous season to be equivalent to this many games
-TRAINING_SEASONS = [2011, 2012, 2013, 2014, 2015, 2016] ## seasons to train model on
+SEASONS = [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020] ## seasons to train model on
 
 ## FUNCTIONS ##
 
@@ -34,11 +40,13 @@ def weight_season_stats(df, previous, current, games_played, weight_previous_sea
 conn, cursor = create_database_connection(PATH_DB)
 
 df_all_seasons = pd.DataFrame({})
-for season in TRAINING_SEASONS:
+for season in SEASONS:
+
+       print(season)
 
        ## query the data for the season
-       df_season = pd.read_sql(f"SELECT * FROM boxscore_processed WHERE season = {season} AND game_type = 2", conn)
-       df_season_prev = pd.read_sql(f"SELECT * FROM boxscore_processed WHERE season = {season-1} AND game_type = 2", conn)
+       df_season = pd.read_sql(f"SELECT * FROM boxscore_processed WHERE season = {season}", conn)
+       df_season_prev = pd.read_sql(f"SELECT * FROM boxscore_processed WHERE season = {season-1}", conn)
 
        ### determine whether this season has any new teams
        teams_all = df_season['home_franchise_id'].unique()
@@ -74,42 +82,69 @@ for season in TRAINING_SEASONS:
        df_missing = pd.DataFrame(columns=means.index)
        for i, missing_team in enumerate(teams_new):
               df_missing.loc[i] = means.values
-              df_missing['team_id'][i] = missing_team
+              df_missing['franchise_id'][i] = missing_team
        df_season_previous = pd.concat([df_season_previous, df_missing])
 
        ### add previous season info to this season's dataset
        df_season = pd.merge(df_season, df_season_previous, left_on = 'home_franchise_id', right_on = 'franchise_id')
        df_season = pd.merge(df_season, df_season_previous, left_on = 'away_franchise_id', right_on = 'franchise_id', suffixes = ('_home','_away'))
 
-       ## add season data into the full dataset
-       df_all_seasons = pd.concat([df_all_seasons, df_season])
 
-df_all_seasons = df_all_seasons.reset_index(drop = True)
+       ## set up the start of the dataset
+       df = deepcopy(df_season[['game_id', 'game_type', 'season', 'datetime', 'away_franchise_id', 'home_franchise_id']])
+       df['home_win'] = df_season['winner'] == 'home'  ## y-variable to be predicted
 
-### BUILD THE DATASET ###
+       ## ADD FEATURES
+       ## wins per game
+       df['wins_per_game_home'] = weight_season_stats(df_season, 'previous_wins_per_game_home', 'home_season_wins', 'home_season_games_played', WEIGHT_PREV_SEASON)
+       df['wins_per_game_away'] = weight_season_stats(df_season, 'previous_wins_per_game_away', 'away_season_wins', 'away_season_games_played', WEIGHT_PREV_SEASON)
 
-## compute variables to be used by the ML model
-goals_for_per_game_home = weight_season_stats(df_all_seasons, 'previous_goals_for_per_game_home', 'home_season_goals_for', 'home_season_games_played', WEIGHT_PREV_SEASON)
-goals_against_per_game_home = weight_season_stats(df_all_seasons, 'previous_goals_against_per_game_home', 'home_season_goals_against', 'home_season_games_played', WEIGHT_PREV_SEASON)
-shots_for_per_game_home = weight_season_stats(df_all_seasons, 'previous_shots_for_per_game_home', 'home_season_shots_for', 'home_season_games_played', WEIGHT_PREV_SEASON)
-shots_against_per_game_home = weight_season_stats(df_all_seasons, 'previous_shots_against_per_game_home', 'home_season_shots_against', 'home_season_games_played', WEIGHT_PREV_SEASON)
-wins_per_game_home = weight_season_stats(df_all_seasons, 'previous_wins_per_game_home', 'home_season_wins', 'home_season_games_played', WEIGHT_PREV_SEASON)
+       ## goals for per game
+       df['goals_for_per_game_home'] = weight_season_stats(df_season, 'previous_goals_for_per_game_home', 'home_season_goals_for', 'home_season_games_played', WEIGHT_PREV_SEASON)
+       df['goals_for_per_game_away'] = weight_season_stats(df_season, 'previous_goals_for_per_game_away', 'away_season_goals_for', 'away_season_games_played', WEIGHT_PREV_SEASON)
 
-goals_for_per_game_away = weight_season_stats(df_all_seasons, 'previous_goals_for_per_game_away', 'away_season_goals_for', 'away_season_games_played', WEIGHT_PREV_SEASON)
-goals_against_per_game_away = weight_season_stats(df_all_seasons, 'previous_goals_against_per_game_away', 'away_season_goals_against', 'away_season_games_played', WEIGHT_PREV_SEASON)
-shots_for_per_game_away = weight_season_stats(df_all_seasons, 'previous_shots_for_per_game_away', 'away_season_shots_against', 'away_season_games_played', WEIGHT_PREV_SEASON)
-shots_against_per_game_away = weight_season_stats(df_all_seasons, 'previous_shots_against_per_game_away', 'away_season_shots_for', 'away_season_games_played', WEIGHT_PREV_SEASON)
-wins_per_game_away = weight_season_stats(df_all_seasons, 'previous_wins_per_game_away', 'away_season_wins', 'away_season_games_played', WEIGHT_PREV_SEASON)
+       ## goals against per game
+       df['goals_against_per_game_home'] = weight_season_stats(df_season, 'previous_goals_against_per_game_home', 'home_season_goals_against', 'home_season_games_played', WEIGHT_PREV_SEASON)
+       df['goals_against_per_game_away'] = weight_season_stats(df_season, 'previous_goals_against_per_game_away', 'away_season_goals_against', 'away_season_games_played', WEIGHT_PREV_SEASON)
 
-## combine stats into the dataset
-df = deepcopy(df_all_seasons[['game_id', 'game_type', 'season', 'datetime']])
-df['home_win'] = df_all_seasons['winner'] == 'home' ## y-variable to be predicted
+       ## shots for per game
+       df['shots_for_per_game_home'] = weight_season_stats(df_season, 'previous_shots_for_per_game_home', 'home_season_shots_for', 'home_season_games_played', WEIGHT_PREV_SEASON)
+       df['shots_for_per_game_away'] = weight_season_stats(df_season, 'previous_shots_for_per_game_away', 'away_season_shots_against', 'away_season_games_played', WEIGHT_PREV_SEASON)
 
-## features
-df['wins_per_game_home'] = wins_per_game_home
-df['wins_per_game_away'] = wins_per_game_away
+       ## shots against per game
+       df['shots_against_per_game_home'] = weight_season_stats(df_season, 'previous_shots_against_per_game_home', 'home_season_shots_against', 'home_season_games_played', WEIGHT_PREV_SEASON)
+       df['shots_against_per_game_away'] = weight_season_stats(df_season, 'previous_shots_against_per_game_away', 'away_season_shots_for', 'away_season_games_played', WEIGHT_PREV_SEASON)
+
+       ## create the table if it doesn't exist
+       execute_query(PATH_QUERIES / 'create_table_mlfeatures', cursor)
+
+       ## query the table in order to skip any existing game
+       df_mlfeatures = pd.read_sql_query(f'SELECT * from mlfeatures WHERE season = {season}', conn)
+       for row in df.iterrows():
+              if row[1]['game_id'] not in df_mlfeatures['game_id'].values:
+                     game_dict = dict(row[1])
+                     execute_query(PATH_QUERIES / 'insert_or_ignore_entry', cursor,
+                                   values=tuple(game_dict.values()),
+                                   replacements={'xtablex' : 'mlfeatures',
+                                                 'xkeysx': ', '.join(list(game_dict.keys())),
+                                                 'xvaluesx': ', '.join(['?'] * len(game_dict.keys()))})
+       conn.commit()
+
+
+
+
+### SAVE TRAIN SET TO CSV ###
+
+TRAINING_SEASONS = [2011, 2012, 2013, 2014, 2015, 2016] ## seasons to train model on
+
+season_string = ', '.join([str(season) for season in TRAINING_SEASONS])
+df_all_seasons = pd.read_sql_query(f'SELECT * from mlfeatures WHERE season IN ({season_string})', conn)
+
 
 ## save
-df.to_csv(PATH_DATA_PROCESSED/'dataset_2021_03_01_wins_only.csv', index = False)
+df_all_seasons.to_csv(PATH_DATA_PROCESSED/'dataset_2021_03_01_wins_only.csv', index = False)
+
+
+
 
 
